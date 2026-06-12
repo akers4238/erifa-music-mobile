175147222149b5f9a1b9d90cd99028b151af27da
akers4238 <akers4238@users.noreply.github.com>
2026-06-12 22:42:07 +0800

    恢复内置搜索源入口

--- src/core/apiSource.ts
+++ src/core/apiSource.ts
@@ -1,4 +1,5 @@
 // import { setUserApi as setUserApiAction } from '@renderer/utils/ipc'
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
 import musicSdk from '@/utils/musicSdk'
 // import apiSourceInfo from '@renderer/utils/musicSdk/api-source-info'
 import { updateSetting } from './common'
@@ -12,6 +13,73 @@ import songlistState from '@/store/songlist/state'
 import leaderboardState from '@/store/leaderboard/state'
 import commonActions from '@/store/common/action'
 
+const builtinSourceEntries = [...musicSdk.sources]
+
+const createMusicSearchListInfo = () => ({
+  page: 1,
+  maxPage: 0,
+  limit: 30,
+  total: 0,
+  list: [],
+  key: null,
+})
+
+const createSongListSearchListInfo = () => ({
+  page: 1,
+  limit: 15,
+  total: 0,
+  list: [],
+  key: null,
+  tagId: '',
+  sortId: '',
+})
+
+const getBuiltinSources = <T extends LX.OnlineSource>(checker: (source: T) => boolean) => {
+  return builtinSourceEntries
+    .map(source => source.id as T)
+    .filter(checker)
+}
+
+const resetBuiltinApiState = () => {
+  const musicSearchSources = getBuiltinSources<LX.OnlineSource>(source => !!musicSdk[source]?.musicSearch)
+  const songListSearchSources = getBuiltinSources<LX.OnlineSource>(source => !!musicSdk[source]?.songList?.search)
+  const songListSources = getBuiltinSources<LX.OnlineSource>(source => !!musicSdk[source]?.songList?.getList)
+  const leaderboardSources = getBuiltinSources<LX.OnlineSource>(source => !!musicSdk[source]?.leaderboard?.getBoards)
+  const hotSearchSources = getBuiltinSources<LX.OnlineSource>(source => !!musicSdk[source]?.hotSearch)
+
+  ;(musicSdk as any).sources = builtinSourceEntries
+  searchState.temp_source = ''
+
+  searchMusicState.source = ''
+  searchMusicState.sources = musicSearchSources.length > 1 ? ['all', ...musicSearchSources] : musicSearchSources
+  searchMusicState.listInfos = {
+    all: createMusicSearchListInfo(),
+    ...Object.fromEntries(musicSearchSources.map(source => [source, createMusicSearchListInfo()])),
+  }
+  searchMusicState.maxPages = {}
+
+  searchSonglistState.source = ''
+  searchSonglistState.sources = songListSearchSources.length > 1 ? ['all', ...songListSearchSources] : songListSearchSources
+  searchSonglistState.listInfos = {
+    all: createSongListSearchListInfo(),
+    ...Object.fromEntries(songListSearchSources.map(source => [source, createSongListSearchListInfo()])),
+  }
+  searchSonglistState.maxPages = {}
+
+  songlistState.sources = songListSources
+  songlistState.sortList = Object.fromEntries(songListSources.map(source => [source, musicSdk[source].songList.sortList ?? []]))
+  songlistState.tags = {}
+
+  leaderboardState.sources = leaderboardSources
+  leaderboardState.boards = {}
+
+  hotSearchState.sources = hotSearchSources.length > 1 ? ['all', ...hotSearchSources] : hotSearchSources
+  hotSearchState.sourceList = {
+    all: [],
+    ...Object.fromEntries(hotSearchSources.map(source => [source, []])),
+  }
+  commonActions.setSourceNames({})
+}
 
 export const setApiSource = (apiId: string) => {
   if (global.lx.apiInitPromise[1]) {
@@ -34,19 +102,7 @@ export const setApiSource = (apiId: string) => {
     // @ts-expect-error
     global.lx.qualityList = musicSdk.supportQuality[apiId] ?? {}
     destroyUserApi()
-    ;(musicSdk as any).sources = []
-    searchState.temp_source = ''
-    searchMusicState.source = ''
-    searchMusicState.sources = []
-    searchSonglistState.source = ''
-    searchSonglistState.sources = []
-    songlistState.sources = []
-    songlistState.sortList = {}
-    songlistState.tags = {}
-    leaderboardState.sources = []
-    leaderboardState.boards = {}
-    hotSearchState.sources = []
-    commonActions.setSourceNames({})
+    resetBuiltinApiState()
     if (!global.lx.apiInitPromise[1]) global.lx.apiInitPromise[2](true)
     // apiSource.value = apiId
     // void setUserApiAction(apiId)
